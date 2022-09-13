5afea648f9385ea1cabf859aca4066ce5d0c8293
Reed Billedo <rjbilledo@tamu.edu>
2022-09-12 22:03:26 -0500

    Added test cases for book assignment

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,36 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('harry potter')
   end
 end
+
+RSpec.describe 'Adding an Author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K.'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Rowling')
+  end
+end
+
+RSpec.describe 'Adding a Price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Price', with: 24.5
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(24.5)
+  end
+end
+
+RSpec.describe 'Adding a Date', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Date', with: Date.new(2022,9,12)
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(Date.new(2022,9,12))
+  end
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -4,10 +4,25 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   subject do
     described_class.new(title: 'harry potter')
+    described_class.new(author: 'J.K.')
+    described_class.new(price: 24.5)
+    described_class.new(date: Date.new(2022,9,12))
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  it 'is valid with valid attributes title' do
+    expect(subject.title).to be_valid
+  end
+
+  it 'is valid with valid attributes author' do
+    expect(subject.author).to be_valid
+  end
+
+  it 'is valid with valid attributes price' do
+    expect(subject.price).to be_valid
+  end
+
+  it 'is valid with valid attributes date' do
+    expect(subject.date).to be_valid
   end
 
   it 'is not valid without a name' do
